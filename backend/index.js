1204d2f8e6dc40b16ff750e4f1d4b048b03ed43e
Rohit Rajendra Barate <rohitbarate100@gmail.com>
2023-03-31 15:32:45 +0530

    setup complete

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 var cors = require('cors') 
 
 connectToMongo();
+
 const app = express()
 const port = 5000
 
--- backend/modules/User.js
+++ backend/modules/User.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
-const {Schema } = mongoose;
+const { Schema } = mongoose;
 
 const Userschema = new Schema({
-  name :{
-      type : String,
-      require : true
-  },
-  email :{
-      type : String,
-      require : true,
-      unique : true
-  },
-  password :{
-      type:String,
-      require : true
-  },
-  date :{
-      type : Date,
-      default : Date.now
-  }
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 const user = mongoose.model('users', Userschema);
